e93b1a267933b7e32f253cd653be58242d619553
Denny Pradipta <denny.aditya.p@gmail.com>
2024-09-04 14:20:53 +0700

    Formatting

--- src/fetch.ts
+++ src/fetch.ts
@@ -20,33 +20,26 @@ const restructureActivities = (data: TActivity[]) => {
       if (activity.type === 'event') {
         if (activity.events_type === 'multiple') {
           const sched = activity.schedules as TEventMultipleScheduleData[]
-          const mapped = sched.map((schedule: TEventMultipleScheduleData) => `
-                Date: ${schedule.date}
-                Start Time: ${schedule.start_time}
-                End Time: ${schedule.end_time}
-          `)
+          const mapped = sched.map((schedule: TEventMultipleScheduleData) => `Date: ${schedule.date}
+Start Time: ${schedule.start_time}
+End Time: ${schedule.end_time}`)
           return mapped.join(`
 `)
         }
 
         if (activity.events_type === 'range') {
           const sched = activity.schedules as TEventRangeScheduleData
-          return `
-                Start Date: ${sched.start_date}
-                End Date: ${sched.end_date}
-                Start Time: ${sched.start_time}
-                End Time: ${sched.end_time}
-          `
+          return `Start Date: ${sched.start_date}
+End Date: ${sched.end_date}
+Start Time: ${sched.start_time}
+End Time: ${sched.end_time}`
         }
       }
 
       if (activity.type === 'project') {
         const sched = activity.schedules as TProjectScheduleData
-        return `
-                Start Date: ${sched.start_date}
-                End Date: ${sched.end_date}
-                
-`
+        return `Start Date: ${sched.start_date}
+End Date: ${sched.end_date}`
       }
 
       if (activity.type === 'regular') {
@@ -54,65 +47,48 @@ const restructureActivities = (data: TActivity[]) => {
         const days = sched.days.map((d: any) => `- ${d.day}, ${d.start_time} - ${d.end_time}`).join(`
 `)
 
-
-        return `
-                Start Date: ${sched.start_date}
-                End Date: ${sched.end_date}
-                Days:
-                ${days}
-`
+        return `Start Date: ${sched.start_date}
+End Date: ${sched.end_date}
+Days:
+${days}`
       }
     }
 
     const getLocation = () => {
       if (activity.location_type === 'fixed') {
         const loc = activity.location_data as TActivityFixedLocationData
-        return `
-                Location:
-                Street: ${loc.street}
-                Zipcode: ${loc.zipcode}
-                Province: ${loc.province}
-                Regency City: ${loc.regency_city}
-`
+        return `Location:
+Street: ${loc.street}
+Zipcode: ${loc.zipcode}
+Province: ${loc.province}
+Regency City: ${loc.regency_city}`
       }
       if (activity.location_type === 'flexible') {
         const loc = activity.location_data as TActivityFlexibleLocationData
-        return `
-                ${loc.location}
-`
+        return `Location:
+${loc.location}`
       }
     }
 
 
     return {
-      pageContent: `
-                Title: ${activity.title}
-
-                Description: ${activity.description}
-
-                Organization: ${activity.organization.name}
-                Focuses: ${activity.organization.focuses.join(', ')}
-
-                Type: ${activity.type}
-
-                Schedule(s):
+      pageContent: `Title: ${activity.title}
+Description: ${activity.description}
+Organization: ${activity.organization.name}
+Focuses: ${activity.organization.focuses.join(', ')}
+Type: ${activity.type}
+Schedule(s):
 ${getSchedules()}
-
-                Divisions:
-            ${activity.divisions
-          .map(
-            (division: any) => `
-                Name: ${division.name}
-                Job Description: ${division.jobdesc}
-                Duration: ${division.volunteering_duration} hours
-                Need to be Prepared: ${division.need_to_be_prepared}`
-          )
+Divisions:
+${activity.divisions.map(
+        (division: any) => `Name: ${division.name}
+Job Description: ${division.jobdesc}
+Duration: ${division.volunteering_duration} hours
+Need to be Prepared: ${division.need_to_be_prepared}`)
           .join(`
-`)
-        }
+`)}
 
-                ${getLocation()}
-            `,
+${getLocation()}`,
       metadata: {
         id: activity._id,
         organization: activity.organization.name,
