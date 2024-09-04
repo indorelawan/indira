4eb7bdc6d4f6247690cc987edeae7344dd94a25a
Denny Pradipta <denny.aditya.p@gmail.com>
2024-09-04 13:22:20 +0700

    Fix parse schedule

--- src/fetch.ts
+++ src/fetch.ts
@@ -10,64 +10,101 @@ type TActivityData = {
 
 const getActivities = async () => {
   const res = await axios.get<TActivityData>(`https://api.festivalrelawan.com/api/activity/ai`);
-  const data = res.data?.results;
+  const data = (res.data as TActivityData).results;
 
   const restructuredData = restructureActivities(data);
   return restructuredData;
 };
 
-const writeToFile = (data: any) => {
-  fs.writeFileSync('activities.json', JSON.stringify(data, null, 2));
-};
 
 const restructureActivities = (data: object[]) => {
-  return data;
+  const activities = data.map((activity: any) => {
 
-  //   console.log(JSON.stringify(data, null, 2));
+    const getSchedules = () => {
+      if (activity.type === 'event') {
+        if (activity.events_type === 'multiple') {
+          const mapped = activity.schedules.map((schedule: any) => `
+                Date: ${schedule.date}
+                Start Time: ${schedule.start_time}
+                End Time: ${schedule.end_time}
+          `)
+          return mapped.join(`
+`)
+        }
 
-  //   const activities = data.map((activity: any) => {
-  //     return {
-  //       pageContent: `
-  //               Title: ${activity.title}
+        if (activity.events_type === 'range') {
+          return `
+                Start Date: ${activity.schedules.start_date}
+                End Date: ${activity.schedules.end_date}
+                Start Time: ${activity.schedules.start_time}
+                End Time: ${activity.schedules.end_time}
+          `
+        }
+      }
 
-  //               Description: ${activity.description}
+      if (activity.type === 'project') {
+        return `
+                Start Date: ${activity.schedules.start_date}
+                End Date: ${activity.schedules.end_date}
+                
+`
+      }
 
-  //               Organization: ${activity.organization.name}
-  //               Focuses: ${activity.organization.focuses.join(', ')}
+      if (activity.type === 'regular') {
+        const days = activity.schedules.days.map((d: any) => `- ${d.day}, ${d.start_time} - ${d.end_time}`).join(`
+`)
 
-  //               Type: ${activity.type}
 
-  //               Schedules:
-  //               Start Date: ${activity.schedules.start_date}, End Date: ${activity.schedules.end_date}
-  //               Days: ${activity.schedules.days
-  //                 .map((day: any) => `Day: ${day.day}, Start: ${day.start_time}, End: ${day.end_time}`)
-  //                 .join('\n')}
+        return `
+                Start Date: ${activity.schedules.start_date}
+                End Date: ${activity.schedules.end_date}
+                Days:
+                ${days}
+`
+      }
+    }
 
-  //               Divisions:
-  //               ${activity.divisions
-  //                 .map(
-  //                   (division: any) =>
-  //                     `Name: ${division.name}\nJob Description: ${division.jobdesc}\nDuration: ${division.volunteering_duration} hours\nNeed to be Prepared: ${division.need_to_be_prepared}`
-  //                 )
-  //                 .join('\n\n')}
 
-  //               Location: ${activity.location_data.location}
-  //           `,
-  //       metadata: {
-  //         id: activity._id,
-  //         organization: activity.organization.name,
-  //         focuses: activity.focuses,
-  //         type: activity.type,
-  //         schedules: activity.schedules,
-  //         registration_deadline: activity.registration_deadline,
-  //         location_type: activity.location_type,
-  //         volunteers_needed: activity.volunteers_needed,
-  //         volunteers: activity.volunteers,
-  //         require_cv: activity.require_cv,
-  //       },
-  //     };
-  //   });
-  //   return activities;
+    return {
+      pageContent: `
+                Title: ${activity.title}
+
+                Description: ${activity.description}
+
+                Organization: ${activity.organization.name}
+                Focuses: ${activity.organization.focuses.join(', ')}
+
+                Type: ${activity.type}
+
+                Schedule(s):
+${getSchedules()}
+
+                Divisions:
+            ${activity.divisions
+          .map(
+            (division: any) =>
+              `Name: ${division.name}\nJob Description: ${division.jobdesc}\nDuration: ${division.volunteering_duration} hours\nNeed to be Prepared: ${division.need_to_be_prepared}`
+          )
+          .join('\n\n')
+        }
+
+                Location: ${activity.location_data.location}
+            `,
+      metadata: {
+        id: activity._id,
+        organization: activity.organization.name,
+        focuses: activity.focuses,
+        type: activity.type,
+        schedules: activity.schedules,
+        registration_deadline: activity.registration_deadline,
+        location_type: activity.location_type,
+        volunteers_needed: activity.volunteers_needed,
+        volunteers: activity.volunteers,
+        require_cv: activity.require_cv,
+      },
+    };
+  });
+  return activities;
 };
 
 export default getActivities;
