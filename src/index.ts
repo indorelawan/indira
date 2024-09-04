035e02540beee73598df9823ad4b6dbcd28c1fd4
muslim.ilmiawan <muslim.ilmiawan@gmail.com>
2024-09-04 12:44:13 +0700

    Add express endpoint

--- src/index.ts
+++ src/index.ts
@@ -17,15 +17,12 @@ const template = `
   You are a helpful assistant from indorelawan. Your task is to help user choose the best activity based on their interests.
   Given the following extracted parts of a long document and a question, create a final answer.
   If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-  ALWAYS return a "CONTEXT" part in your answer.
 
   ========
-  CONTEXT:
-  {context}
+  CONTEXT: {context}
   ========
-
   QUESTION: {question}
-  =========
+  ========
   ANSWER:
 `;
 
@@ -33,15 +30,32 @@ const prompt = ChatPromptTemplate.fromTemplate(template);
 
 const chain = prompt.pipe(llm);
 
-async function main() {
+async function chat(question: string) {
   const activities = await getActivities();
   //   console.log(JSON.stringify(activities, null, 2));
   const res = await chain.invoke({
     context: JSON.stringify(JSON.stringify(activities, null, 2)),
-    question: 'What is the best activity to do in indorelawan?',
+    question: question,
   });
 
-  console.log(res);
+  return res;
 }
 
-main();
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/chat', async (req, res) => {
+  const question = req.query.question as string;
+  const response = await chat(question);
+  res.send(response.content);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
